1712b661ca32ecd271ac92ef8c1e442447f8dfea
desentcare <desentcare45@gmail.com>
2021-03-18 01:42:14 +0500

    convert indentation to tabs

--- bot.py
+++ bot.py
@@ -3,24 +3,24 @@ import handlers
 import sqlite3
 import telegram
 from telegram.ext import (
-    Updater,
-    ConversationHandler,
-    MessageFilter,
-    MessageHandler,
-    CommandHandler,
-    Filters,
-    CallbackContext,
+	Updater,
+	ConversationHandler,
+	MessageFilter,
+	MessageHandler,
+	CommandHandler,
+	Filters,
+	CallbackContext,
 )
 
 updater = Updater(token=config.TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
 for h in handlers.handlers:
-    dispatcher.add_handler(h)
+	dispatcher.add_handler(h)
 
 def main():
-    updater.start_polling()
-    updater.idle()
+	updater.start_polling()
+	updater.idle()
 
 if __name__=="__main__":
-    main()
+	main()
